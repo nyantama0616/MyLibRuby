44c824eeef0f04a25c079f5b451caf902e76f1d7
nyantama <r_nyantama0616@yahoo.co.jp>
2021-03-29 19:11:05 +0900

    [重要!] ヒープ構造の欠陥が発覚！！addメソッドの中身を修正した。これで多分大丈夫。 Math系メソッドを新たにいくつか追加。 Binary Indexed Tree を新たに実装！！！

--- math/math.rb
+++ math/math.rb
@@ -7,7 +7,7 @@ def make_divisors(num)
       result << num / x
     end
   end
-  result.uniq.sort
+  result.uniq.sort #状況に応じて、uniqを外す
 end
 
 # 度数法　→　弧度法
@@ -29,19 +29,70 @@ def imos!(array)
   end
 end
 
-
 #配列の各要素の分母を払う(最も簡単な整数にする)
-def to_easy(array)
-  x = array.inject(1) {|r, v| r.lcm(v.denominator)}
-  array = array.map {|a| (a * x).to_i}
+def to_easy!(rational_array)
+  x = rational_array.inject(1) {|r, v| r.lcm(v.denominator)}
+  rational_array = rational_array.map {|a| (a * x).to_i}
 end
+
+# 最頻値を求める(ソート済みの配列を渡す)
+def mode(array, &comp)
+  comp = comp || proc {|a, b| a <=> b}
+  
+  result = array[0]
+  count_max = 0
+  
+  count = 0
+  (1...array.size).each do |i|
+    if comp.call(array[i], array[i - 1]).eql?(0)
+      count += 1
+    else
+      if count > count_max
+        count_max = count
+        result = array[i - 1]
+      end
+      count = 0
+    end
+  end
+  
+  if count > count_max
+        count_max = count
+        result = array[-1]
+  end
+
+  result
+end  
+
+# 中央値を求める（ソート済みの配列を渡す）
+def midian(sorted_array)
+    len = sorted_array.length
+    if len < 2
+        return sorted_array[0]
+    end
+
+    if len.even?
+        (sorted_array[len / 2] + sorted_array[len / 2 - 1]) / 2
+    else
+        sorted_array[len / 2]
+    end
+end
+
+# 組み合わせの数を計算
+def binomial(n, r)
+  [r.times.inject(1) {|a, b| a * (n - b)} / (1..r).inject(1, :*), 0].max
+end  
+
 
 
 # -------------------- ２点間系 ----------------------
 
-# 距離
-def dist(m1, m2)
-  Math.hypot(m1[0] - m2[0], m1[1] - m2[1])
+# ユークリッド距離 or マンハッタン距離
+def dist(m1, m2, mh: false)
+  if mh
+    (m1[0] - m2[0]).abs + (m1[1] - m2[1]).abs #マンハッタン
+  else
+    Math.hypot(m1[0] - m2[0], m1[1] - m2[1]) # ユークリッド
+  end
 end
 
 # ２点を通る直線の方程式(ax + bx = c)
@@ -57,8 +108,43 @@ def equ_2(p1, p2)
   to_easy(result)
 end
 
+# ----チェック系----
 
+# 整数判定
+def int?(num)
+  num - num.to_i.eql?(0)
+end
 
+# ------雑魚関数-----
 
+# 数字を日本語へ(ex: 15000 →　1万5000)
+def to_j(num)
+    list = %w(万 億 兆 京 垓 杼 穣 溝 潤 正 載 極 恒河沙 阿僧祇 那由他 不可思議 無量大数)
+    d = Math.log10(num).floor + 1
+    num = num.to_s.reverse
+    p num
+    result = ""
+    temp = []
+    d.downto(1) do |i|
+    temp << num[i - 1]
+        if i % 4 == 1 && i != 1
+            p temp
+            result += temp.join.to_i.to_s + list[i / 4 - 1]
+            temp.clear
+        end
+    end
 
-# -------- 数学じゃないやつ ------------
+    result += temp.join.to_i.to_s unless temp.all? {|c| c == "0"}
+    result
+end
+
+  # 符号取得
+  def sign(x)
+      if x > 0
+          1 
+      elsif x < 0
+          -1
+      else
+          0
+      end
+  end
--- structure/structure.rb
+++ structure/structure.rb
@@ -1,69 +1,80 @@
 # ヒープ構造
 class Heap
     attr_accessor :cont
-  def initialize(&comp)
-    @cont = []
-    @comp = comp || proc {|a, b| a <=> b}
-  end
-
-#   要素追加
-  def add(x)
-    @cont << x
-    i = @cont.size - 1
-
-    while true
-        j = i / 2
-        if @comp.call(@cont[i], @cont[j]) < 0
-            @cont[i], @cont[j] = @cont[j], @cont[i]
-            i, j = j, j / 2
-        else
-            break
-        end
+    def initialize(&comp)
+        @cont = []
+        @comp = comp || proc {|a, b| a <=> b}
     end
-  end
 
-#   要素取得
-  def remove
-    result = @cont.shift
-    if temp = @cont.pop
-        @cont.unshift(temp)
+    #   要素追加
+    def add(x)
+        @cont << x
+        i = @cont.size - 1
+        j = i.odd? ? i / 2 : i / 2 - 1
 
-        # 要素取得後のヒープ再構築
-        i = 0
-        left = 1
-        right = 2
         while true
-            if @cont[left] && @cont[right]
-                if @comp.call(@cont[left], @cont[right]) <= 0 && @comp.call(@cont[left], @cont[i]) < 0
-                    @cont[i], @cont[left] = @cont[left], @cont[i]
-                    i = left
-                    left, right = 2*left + 1, 2*left + 2
-                elsif @comp.call(@cont[right], @cont[left]) < 0 && @comp.call(@cont[right], @cont[i]) < 0
-                    @cont[i], @cont[right] = @cont[right], @cont[i]
-                    i = right
-                    left, right = 2*right + 1, 2*right + 2
-                else
-                    break
-                end
-            elsif @cont[left] && @comp.call(@cont[left], @cont[i]) < 0
-                    @cont[i], @cont[left] = @cont[left], @cont[i]
-                    break
+            break if j < 0
+            if @comp.call(@cont[i], @cont[j]) < 0
+                @cont[i], @cont[j] = @cont[j], @cont[i]
+                i, j = j, j.odd? ? j / 2 : j / 2 - 1
             else
                 break
             end
         end
     end
-    result
-  end
+
+    #   要素取得
+    def remove
+        result = @cont.shift
+        if temp = @cont.pop
+            @cont.unshift(temp)
+
+            # 要素取得後のヒープ再構築
+            i = 0
+            left = 1
+            right = 2
+            while true
+                if @cont[left] && @cont[right]
+                    if @comp.call(@cont[left], @cont[right]) <= 0 && @comp.call(@cont[left], @cont[i]) < 0
+                        @cont[i], @cont[left] = @cont[left], @cont[i]
+                        i = left
+                        left, right = 2*left + 1, 2*left + 2
+                    elsif @comp.call(@cont[right], @cont[left]) < 0 && @comp.call(@cont[right], @cont[i]) < 0
+                        @cont[i], @cont[right] = @cont[right], @cont[i]
+                        i = right
+                        left, right = 2*right + 1, 2*right + 2
+                    else
+                        break
+                    end
+                elsif @cont[left] && @comp.call(@cont[left], @cont[i]) < 0
+                        @cont[i], @cont[left] = @cont[left], @cont[i]
+                        break
+                else
+                    break
+                end
+            end
+        end
+        result
+    end
+
+    #先頭要素を返す
+    def peek
+        @cont.first
+    end
+
+    #空かどうか確認
+    def empty?
+        @cont.empty?
+    end
 end
 
-#-------------------------------------------------
 
-# 平衡二分探索木(ピボットツリー)(1...size)の値を格納できる
+
+# 平衡二分探索木(ピボットツリー)(1...size)の値を格納できる (参考: https://qiita.com/Kiri8128/items/6256f8559f0026485d90)
 class PibotTree
     attr_accessor :root, :size, :k, :comp
 
-    def initialize(size, &comp)
+    def initialize(size, &comp) #場合に応じて、比較方法をブロックで渡す
         @size = size
         @k = (size > 3) ? Math.log2(size).ceil : Math.log2(size).floor + 1
         @root = Node.new(2**(@k - 1))
@@ -88,7 +99,7 @@ class PibotTree
             if @comp.call(x, node.pibot) < 0 || (@comp.call(x, node.pibot) == 0 && comp.call(node.value, node.pibot) < 0)
                 x, node.value = temp
                 node = node.left
-            else #@comp.call(x, node.pibot) > 0 || (@comp.call(x, node.pibot) == 0 &&  comp.call(node.value, node.pibot) > 0)
+            else
                 node.value, x = temp
                 node = node.right
             end
@@ -177,4 +188,97 @@ class Node
 
 end
 
-# ------------------
+
+
+# BIT
+class BinaryIndexedTree
+    attr_accessor :bits, :len
+    
+    # 数列aryの全要素が0以上である必要がある
+    def initialize(ary)
+        @bits = ary.map.with_index {|x, i| Bit.new(x, i + 1)}
+        @len = ary.length
+    end
+
+    # すべての区間和を計算
+    def set_interval_sum
+        @bits.each_with_index do |bit, i|
+            bit.interval_sum = bit.value
+            d = 1
+            while d < bit.lsb
+                bit_ = @bits[i - d]
+                bit.interval_sum += bit_.interval_sum
+                d += bit_.lsb
+            end
+        end
+    end
+
+    # 値の更新
+    def update_at(i, value)
+        bit = @bits[i]
+        diff = bit.value - value
+        bit.value = value
+        while i < len
+            bit = bits[i]
+            bit.interval_sum -= diff
+            i += bit.lsb
+        end
+    end
+    
+    # a_iをvalueだけインクリメントする
+    def increase_at(i, value)
+        bit = @bits[i]
+        bit.value += value
+        while i < len
+            bit = bits[i]
+            bit.interval_sum += value
+            i += bit.lsb
+        end
+    end
+
+    # a_1 ~ a_iまでの総和を計算
+    def calc_sum_at(i)
+        result = 0
+        while i >= 0
+            bit = @bits[i]
+            result += bit.interval_sum
+            i -= bit.lsb
+        end
+        result
+    end
+
+     # a_1 ~ a_iまでの総和がx以下になるiの最大値
+    def get_min_i(x)
+        result = nil
+        d = 2 ** Math.log2(@len).floor
+        i = d - 1
+        while bit = @bits[i]
+            bit = @bits[i]
+            d /= 2
+            if calc_sum_at(i) <= x
+                result = i
+                i += d
+            else
+                i -= d
+            end
+            break if bit.lsb == 1
+        end 
+        result
+    end
+
+    # 確認用
+    def show_bits
+        p @bits.map {|x| x.value}
+        p @bits.map {|x| x.interval_sum}
+        p @bits.map {|x| x.lsb}
+    end
+end
+
+class Bit
+    attr_accessor :value, :interval_sum, :lsb
+
+    def initialize(value, i)
+        @value = value
+        @lsb = i & -i
+    end
+end
